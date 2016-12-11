be6213dfbc9627f21345946bd429b56cbbdb4d90
ZahARin <lavysh.zahar@gmail.com>
2016-12-11 17:35:34 +0200

    first

---
+++ app/src/main/java/com/study/zaharin/stopwatch/StopwatchActivity.java
@@ -0,0 +1,16 @@
+package com.study.zaharin.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+
+        TextView view;
+    }
+}
